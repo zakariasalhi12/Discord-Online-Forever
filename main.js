9a9b2840e694232cb248a9935057efa7987fcfa9
zakaria salhi <106976277+zakariasalhi12@users.noreply.github.com>
2024-11-08 18:07:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
-const { Client } = require("discord.js-selfbot-v13")
-const { joinVoiceChannel } = require("@discordjs/voice")
-const { TOKEN, GUILDID, CHANNELEID, selfDeaf, selfMute } = require("./config.json")
-
-const client = new Client()
-
-client.on("ready", async () => {
-    console.log(`Logged in as ${client.user.tag}`);
+const { Client } = require("discord.js-selfbot-v13");
+const { joinVoiceChannel } = require("@discordjs/voice");
+const { TOKENS, GUILDID, CHANNELEID, selfDeaf, selfMute } = require("./config.json");
 
 
-    setInterval(async () => {
-        try {
-            await joinVoiceChannel({
-                channelId: CHANNELEID,
-                guildId: GUILDID,
-                adapterCreator: client.guilds.cache.get(GUILDID).voiceAdapterCreator,
-                selfDeaf: selfDeaf,
-                selfMute: selfMute,
-            });
-        } catch (error) {
-            console.error("Failed to join the voice channel:", error);
-        }
-    }, 5000);
-    
-});
+for (let index = 0; index < TOKENS.length; index++) {
+    setTimeout(() => {
+        const client = new Client();
 
+        client.on("ready", async () => {
+            console.log(`Logged in as ${client.user.tag}`);
 
+            try {
+                const connection = joinVoiceChannel({
+                    channelId: CHANNELEID,
+                    guildId: GUILDID,
+                    adapterCreator: client.guilds.cache.get(GUILDID)?.voiceAdapterCreator,
+                    selfDeaf: selfDeaf,
+                    selfMute: selfMute,
+                    group:`${index}`
+                });
+                console.log(`Joined voice channel with ${client.user.tag}`);
 
-client.login(TOKEN)
+                connection.on("stateChange", (oldState, newState) => {
+                    if (oldState.status === "connected" && newState.status !== "connected") {
+                        console.log(`Rejoining voice channel for ${client.user.tag}`);
+                        joinVoiceChannel({
+                            channelId: CHANNELEID,
+                            guildId: GUILDID,
+                            adapterCreator: client.guilds.cache.get(GUILDID).voiceAdapterCreator,
+                            selfDeaf: selfDeaf,
+                            selfMute: selfMute,
+                        });
+                    }
+                });
+            } catch (error) {
+                console.error(`Failed to join voice channel for ${client.user.tag}:`, error);
+            }
+        });
+
+        client.login(TOKENS[index]).catch((error) => {
+            console.error(`Failed to log in with token ${TOKENS[index]}:`, error);
+        });
+    }, index * 1000)
+}
